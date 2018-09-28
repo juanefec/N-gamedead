9d75527bd63991145b92e3e6b5f9d658aae945ae
Lucas Amado <lucasa@lagash.com>
2018-09-28 13:54:06 -0300

    feat: add models

---
+++ server/gameWorld.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"./model"
+)
+
+type GameWorld struct {
+	Players []model.Player
+}
---
+++ server/model/player.go
@@ -0,0 +1,6 @@
+package model
+
+type Player struct {
+	Name     string
+	Position Position
+}
